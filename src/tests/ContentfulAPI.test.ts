1ba5832427baf54872cada06906cdc2aa62ff7c9
bramirez96 <bran.ramirez.don@gmail.com>
2024-04-18 15:31:44 -0700

    Add in a basic test. Use the latest version of the api library.

---
+++ src/tests/ContentfulAPI.test.ts
@@ -0,0 +1,15 @@
+// ! Copyright (c) 2024, Brandon Ramirez, brr.dev
+
+import { ContentfulAPI } from "../index";
+
+describe("ContentfulAPI tests", () => {
+    it("builds without error", () => {
+        expect(() => {
+            new ContentfulAPI({
+                token: "faketoken",
+                spaceID: "fakespaceid",
+                environment: "fakeenv",
+            });
+        }).not.toThrow();
+    });
+});
